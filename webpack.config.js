7c161c30c2359acf01cf253672e341cd09876e86
zhuxl <2449514486@qq.com>
2020-03-23 10:12:51 +0800

    Using webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ module.exports = {
         print: './src/print.js',
     },
     devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
